d180ff033ff12384aeca275488000b5af54da9f3
sussybakka148-hash <sussybakka148@gmail.com>
2026-03-16 14:52:12 +0530

    UC5 first commit

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
@@ -8,7 +9,7 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string to check: ");
         String input = scanner.nextLine();
 
-        if (checkPalindrome(input)) {
+        if (checkPalindromeUsingStack(input)) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
@@ -17,24 +18,23 @@ public class PalindromeCheckerApp {
         scanner.close();
     }
 
-    public static boolean checkPalindrome(String str) {
+    public static boolean checkPalindromeUsingStack(String str) {
 
         // Remove spaces and convert to lowercase
         String cleanStr = str.replaceAll("\\s+", "").toLowerCase();
 
-        // Convert string to character array
-        char[] chars = cleanStr.toCharArray();
+        Stack<Character> stack = new Stack<>();
 
-        // Two pointer approach
-        int start = 0;
-        int end = chars.length - 1;
+        // Push characters into stack
+        for (char c : cleanStr.toCharArray()) {
+            stack.push(c);
+        }
 
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        // Compare with popped characters
+        for (int i = 0; i < cleanStr.length(); i++) {
+            if (cleanStr.charAt(i) != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
 
         return true;
