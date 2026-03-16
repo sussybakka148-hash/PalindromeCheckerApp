bd8bc8269b35750d6bf7333e6bfefd0aebe73c0a
sussybakka148-hash <sussybakka148@gmail.com>
2026-03-16 14:57:37 +0530

    UC6 first commit

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a string to check: ");
         String input = scanner.nextLine();
 
-        if (checkPalindromeUsingStack(input)) {
+        if (checkPalindromeUsingQueueStack(input)) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
@@ -18,21 +21,27 @@ public class PalindromeCheckerApp {
         scanner.close();
     }
 
-    public static boolean checkPalindromeUsingStack(String str) {
+    public static boolean checkPalindromeUsingQueueStack(String str) {
 
         // Remove spaces and convert to lowercase
         String cleanStr = str.replaceAll("\\s+", "").toLowerCase();
 
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
-        // Push characters into stack
+        // Push into stack and enqueue into queue
         for (char c : cleanStr.toCharArray()) {
             stack.push(c);
+            queue.add(c);
         }
 
-        // Compare with popped characters
-        for (int i = 0; i < cleanStr.length(); i++) {
-            if (cleanStr.charAt(i) != stack.pop()) {
+        // Compare dequeue vs pop
+        while (!stack.isEmpty()) {
+
+            char fromStack = stack.pop();
+            char fromQueue = queue.remove();
+
+            if (fromStack != fromQueue) {
                 return false;
             }
         }
